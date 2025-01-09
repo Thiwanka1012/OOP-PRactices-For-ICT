86ad255e866cefbbed3aee434a2c520088b4ef85
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-01-09 20:41:32 +0530

    Area And Volume

---
+++ Day1/Exmple1.java
@@ -0,0 +1,28 @@
+public class Exmple1 {
+    public static void main(String[] args) {
+
+        //Variable Diclaration
+        int length;
+        int width;
+        int height;
+
+
+        int volume;
+        int area;
+
+        //Values Initialisazion
+
+        length=3;
+        width=5;
+        height=8;
+
+        //Volume
+
+        volume= length*width*height;
+        System.out.println(volume);
+
+        //Area
+        area=2*length*width+2*length*height+2*width*height;
+        System.out.println(area);
+    }
+}
