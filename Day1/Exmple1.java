fb55be34dbdf1dcfd34c8fd9b8632e245c5c5b97
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-01-10 11:57:57 +0530

    Classes

--- Day1/Exmple1.java
+++ Day1/Exmple1.java
@@ -1,28 +1,49 @@
+class Box{
+    int length;
+    int width;
+    int height;
+
+    public void setSize(int l,int h,int w){
+        length=l;
+        width=w;
+        height=h;
+
+    }
+
+    public void printVolume(){
+        int volume;
+        volume=length*width*height;
+        System.out.println("Volume is"+volume);
+    }
+
+
+    public void printArea(){
+        int area;
+        area=2*length*width+2*width*height+2*width*length;
+        System.out.println("Area is"+area);
+    }
+
+
+}
+
+
+
+
 public class Exmple1 {
     public static void main(String[] args) {
+        Box b1=new Box();
+        b1.setSize(10,20,30);
 
-        //Variable Diclaration
-        int length;
-        int width;
-        int height;
+        b1.printArea();
+        b1.printVolume();
 
 
-        int volume;
-        int area;
 
-        //Values Initialisazion
+        Box b2=new Box();
+        b2.setSize(20,60,80);
 
-        length=3;
-        width=5;
-        height=8;
+        b2.printArea();
+        b2.printVolume();
 
-        //Volume
-
-        volume= length*width*height;
-        System.out.println(volume);
-
-        //Area
-        area=2*length*width+2*length*height+2*width*height;
-        System.out.println(area);
     }
 }
